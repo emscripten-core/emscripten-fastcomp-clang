604c8c1da5a7e986ef67212c2999d10961b02a56
Jacob Gravelle <jgravelle@google.com>
2016-08-30 11:32:39 -0700

    Add "override" to silence some compiler warnings

--- lib/Basic/Targets.cpp
+++ lib/Basic/Targets.cpp
@@ -7707,10 +7707,10 @@ public:
     // Reuse PNaCl's va_list lowering.
     return TargetInfo::PNaClABIBuiltinVaList;
   }
-  ArrayRef<const char *> getGCCRegNames() const {
+  ArrayRef<const char *> getGCCRegNames() const override {
     return None;
   }
-  ArrayRef<TargetInfo::GCCRegAlias> getGCCRegAliases() const {
+  ArrayRef<TargetInfo::GCCRegAlias> getGCCRegAliases() const override {
     return None;
   }
   bool validateAsmConstraint(const char *&Name,
