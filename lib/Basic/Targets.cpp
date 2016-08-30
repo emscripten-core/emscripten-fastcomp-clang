11309e17913197d3dce34af67b945e9d1cc79c86
Jacob Gravelle <jgravelle@google.com>
2016-08-30 11:25:13 -0700

    Modify AsmJS SizeType to match Wasm
    AsmJS uses unsigned ints for size_t, which causes some libcxx functions
    to have different symbols in AsmJS than with Wasm, which uses unsigned
    longs.
    In principle we want AsmJS and Wasm to have the same ABI, so this serves
    as a first step toward ensuring that they match.

--- lib/Basic/Targets.cpp
+++ lib/Basic/Targets.cpp
@@ -7685,9 +7685,6 @@ public:
     IntMaxType = Int64Type = TargetInfo::SignedLongLong;
     DoubleAlign = 64;
     LongDoubleWidth = LongDoubleAlign = 64;
-    SizeType = TargetInfo::UnsignedInt;
-    PtrDiffType = TargetInfo::SignedInt;
-    IntPtrType = TargetInfo::SignedInt;
     RegParmMax = 0; // Disallow regparm
 
     // Set the native integer widths set to just i32, since that's currently the
